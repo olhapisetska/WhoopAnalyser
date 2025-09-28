b2bc9282650827937701feba40280b5e337819ef
olhapisetska <olapisetska@gmail.com>
2025-09-28 19:32:01 +0200

    edits

--- user2.py
+++ user2.py
@@ -5,6 +5,4 @@ load_dotenv()
 
 CLIENT_ID = os.getenv("WHOOP_CLIENT_ID")
 CLIENT_SECRET = os.getenv("WHOOP_CLIENT_SECRET")
-#USERNAME = os.getenv("WHOOP_USERNAME")
-#PASSWORD = os.getenv("WHOOP_PASSWORD")
 REDIRECT_URI=os.getenv("WHOOP_REDIRECT_URI")
